621823efcba2529c61c3e417744a0720c1d70d20
plla2 <jhbljs92@naver.com>
2024-11-15 10:00:57 +0900

    Feat: 3002포트로 정적 파일 서버 작성

---
+++ server.ts
@@ -0,0 +1,32 @@
+const server = Bun.serve({
+  port: 3002,
+  fetch: async (request) => {
+    console.log();
+    const url = new URL(request.url);
+
+    if (url.pathname === '/') {
+      const html = await Bun.file('index.html').text();
+      const response = new Response(html, {
+        status: 200,
+        headers: {
+          ['Content-Type']: 'text/html',
+        },
+      });
+      return response;
+    }
+
+    const js = await Bun.file(url.pathname.slice(1))
+      .text()
+      .catch(() => Bun.file(url.pathname.slice(1) + '.js').text());
+    const response = new Response(js, {
+      status: 200,
+      headers: {
+        ['Content-Type']: 'application/javascript',
+      },
+    });
+
+    return response;
+  },
+});
+
+console.log(`${server.port}로 포트 열림`);
